affa10aadf087fba7988ddf6b412e2fdf314862d
Wei Ouyang <oeway007@gmail.com>
2023-11-30 17:31:41 +0100

    add async openai

--- scripts/test_gpt4_vision.py
+++ scripts/test_gpt4_vision.py
@@ -2,27 +2,26 @@
 
 # https://platform.openai.com/docs/guides/vision
 
-from openai import OpenAI
+from openai import AsyncOpenAI
 
-client = OpenAI()
-
-response = client.chat.completions.create(
-  model="gpt-4-vision-preview",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": "What’s in this image?"},
+async def aask():
+    client = AsyncOpenAI()
+    response = await client.chat.completions.create(
+      model="gpt-4-vision-preview",
+      messages=[
         {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-          },
-        },
+          "role": "user",
+          "content": [
+            {"type": "text", "text": "What’s in this image?"},
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+              },
+            },
+          ],
+        }
       ],
-    }
-  ],
-  max_tokens=300,
-)
-
-print(response.choices[0])
+      max_tokens=300,
+    )
+    return response.choices[0]
